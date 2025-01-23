164474e23e936e30f873e1cdcf40d57c3302b834
Dmitry Starostenko <dzmitry.starastsenka@paysera.net>
2025-01-24 00:58:37 +0300

    wip: contest markup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { PromoEfesGeComponent } from './shared/components/promoEfesGe/promoEfesG
 import { LoginComponent } from './shared/components/login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: PromoEfesGeComponent, title: 'EFES Georgia Contest' },
-    { path: 'login', component: LoginComponent, title: 'Arttify - Login' },
+    { path: '', component: PromoEfesGeComponent, title: 'ARttify - EFES Georgia Contest' },
+    { path: 'login', component: LoginComponent, title: 'ARttify - Login' },
     { path: '**', redirectTo: '' }
 ];
--- src/app/shared/components/promoEfesGe/promoEfesGe.component.ts
+++ src/app/shared/components/promoEfesGe/promoEfesGe.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
 
+import { IconComponent } from "../Icon/icon.component";
+import { IconKeys } from "../Icon/const";
+
 @Component({
     selector: 'promo-efes-ge',
     templateUrl: './promoEfesGe.component.html',
+    imports: [IconComponent],
 })
+
 export class PromoEfesGeComponent {
+    icons = IconKeys;
 }
